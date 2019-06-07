afb0945baf0f0d966691d9e1c2230632c4a93404
Sprengzor <otto.spreng@sou.unifeob.edu.br>
2019-06-06 21:31:46 -0300

    Updates

---
+++ precache-manifest.5095dee38f7d282d2db6332a6da14832.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7629dcdb79d4d5986ad9cf15a0354a9",
+    "url": "/workshop-exemplo/index.html"
+  },
+  {
+    "revision": "102746d2eb742753e2de",
+    "url": "/workshop-exemplo/static/css/main.b9d3997c.chunk.css"
+  },
+  {
+    "revision": "0fbf26bc092b7c1ddf99",
+    "url": "/workshop-exemplo/static/js/2.e3379c23.chunk.js"
+  },
+  {
+    "revision": "102746d2eb742753e2de",
+    "url": "/workshop-exemplo/static/js/main.86902ef5.chunk.js"
+  },
+  {
+    "revision": "1fbf5eb1408bb02f7609",
+    "url": "/workshop-exemplo/static/js/runtime~main.37997524.js"
+  }
+]);
